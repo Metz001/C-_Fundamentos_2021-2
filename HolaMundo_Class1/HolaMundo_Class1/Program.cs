d0b425862dd2c0f570cd68b36e697bbb9b737d68
Metz001 <46971952+Metz001@users.noreply.github.com>
2021-12-06 14:52:36 -0500

    Mod de comentarios

--- HolaMundo_Class1/HolaMundo_Class1/Program.cs
+++ HolaMundo_Class1/HolaMundo_Class1/Program.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/*
+ * Programa: Hola Mundo
+ * 
+ * Descripción:
+ * ->Éste codigo demuestra la impresión de mensajes básicos en consola usando el método
+ * Console.WriteLine() una vez es ejecutado o depurado el programa
+ * 
+ * Declaramos un par de variables tipo "string" que son cadena de caracteres
+*/
+using System;
 
 
 namespace HolaMundo_Class1
@@ -7,18 +16,23 @@ namespace HolaMundo_Class1
     {
         static void Main()
         {
-            Console.WriteLine("Bienvenido");
-            //Tipo de varible - nombre -= a lo que es igual la viariable
-
-
+            Console.WriteLine("Bienvenido"); //← Invocación sencilla de Console.WriteLine 
+                              //↑El mensaje va entre paréntesis y comillas dobles porque es un string o texto
+           
+            //declaración de una variable
             string mensaje;
+            //↑ tipo de varibale + nombre de variable
             mensaje = "Esto es un mensaje";
+            //↑ Asignación de valor de variable. Llamamos la varibale por su nombre y la igualamos(=) a lo que queremos
             Console.WriteLine(mensaje + "\n");
-
+            //↑ Impresión en consola de la varibale "mensaje" y salto de línea con el comando "\n"
             string otroMensaje = "Esto es otro mensaje";
-            Console.WriteLine(otroMensaje + " de Hola Mundo" + "\n");
 
+            Console.WriteLine(otroMensaje + " de Hola Mundo" + "\n");
+            //↑ Impresión en consola de la varibale "otroMensaje" y concatenación (+) con un mensaje que dice:
+            //" de hola mundo"
             Console.WriteLine(mensaje + " - " +otroMensaje);
+            //Impresión de dos variables
             Console.WriteLine();
            
         }
